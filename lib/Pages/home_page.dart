49970e02ebeaf29d94384cf7fb830439d2e8edba
Saksham <sakshamloona9@gmail.com>
2024-12-11 22:40:09 +0530

    Text Update

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -6,13 +6,20 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _moonImage(),
+      body: _moonText(),
     );
   }
 
-  //Widget _moonText() {
-
-  //}
+  Widget _moonText() {
+    return const Text(
+      "#GoMoon",
+      style: TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.w800,
+        fontSize: 70,
+      ),
+    );
+  }
 
   Widget _moonImage() {
     return Container(
